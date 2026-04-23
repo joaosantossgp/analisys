2b4307067d38467f419fe306ef273f766b542131
jadaojoao <jaotrab@gmail.com>
2026-04-23 20:04:02 -0300

    fix(frontend): unblock portfolio PR governance

--- apps/web/start-web-wt.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-set "MAIN_WEB=C:\Users\jadaojoao\Documents\analisys\apps\web"
-set "NODE_PATH=%MAIN_WEB%\node_modules"
-"%MAIN_WEB%\node_modules\.bin\next" dev --port 3002 --no-turbopack
